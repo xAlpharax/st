8d06917df748f961943870eb102d5f283737ce19
xAlpharax <42233094+xAlpharax@users.noreply.github.com>
2023-08-14 03:02:26 +0300

    Cleaned up the repository and patched boxdraw successfully.
    Changes to be committed:
    	modified:   Makefile
    	new file:   Makefile.orig
    	deleted:    boxdraw.c
    	deleted:    boxdraw_data.h
    	modified:   config.def.h.orig
    	deleted:    config.def.h.rej
    	modified:   st.c
    	modified:   st.c.orig
    	deleted:    st.c.rej
    	modified:   st.h
            modified:   st.h.orig
            deleted:    st.h.rej
    	deleted:    st.o
    	modified:   x.c
    	modified:   x.c.orig
    	deleted:    x.o

--- st.h
+++ st.h
@@ -33,7 +33,6 @@ enum glyph_attribute {
 	ATTR_WRAP       = 1 << 8,
 	ATTR_WIDE       = 1 << 9,
 	ATTR_WDUMMY     = 1 << 10,
-	ATTR_BOXDRAW    = 1 << 11,
 	ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
 };
 
@@ -115,14 +114,6 @@ void *xmalloc(size_t);
 void *xrealloc(void *, size_t);
 char *xstrdup(const char *);
 
-int isboxdraw(Rune);
-ushort boxdrawindex(const Glyph *);
-#ifdef XFT_VERSION
-/* only exposed to x.c, otherwise we'll need Xft.h for the types */
-void boxdraw_xinit(Display *, Colormap, XftDraw *, Visual *);
-void drawboxes(int, int, int, int, XftColor *, XftColor *, const XftGlyphFontSpec *, int);
-#endif
-
 /* config.h globals */
 extern char *utmp;
 extern char *scroll;
@@ -136,5 +127,5 @@ extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
 extern unsigned int defaultcs;
-extern float alpha;
 extern const int boxdraw, boxdraw_bold, boxdraw_braille;
+extern float alpha;
